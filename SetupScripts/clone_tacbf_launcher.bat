ee5e548530a91d45212f47eb6345964da7539ae5
Lukasz Taczuk <overfl0@users.noreply.github.com>
2015-06-10 23:35:14 +0200

    Updated launcher repository url

--- SetupScripts/clone_tacbf_launcher.bat
+++ SetupScripts/clone_tacbf_launcher.bat
@@ -3,5 +3,5 @@
 echo "Preparing to clone the repository"
 set /P username="Enter your bitbucket username: "
 
-"C:\Program Files\Git\bin\git.exe" clone https://%username%@bitbucket.org/sighter/tacbf-launcher.git
+"C:\Program Files\Git\bin\git.exe" clone https://%username%@bitbucket.org/tacbf_launcher/tacbf_launcher.git
 pause
