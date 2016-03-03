4c62ac64dca40c6f3e2fc76f221ee4578c89a19e
Lukasz Taczuk <overfl0@users.noreply.github.com>
2016-03-03 14:26:30 +0100

    Minor batch fix

--- winrm/runme.bat
+++ winrm/runme.bat
@@ -2,9 +2,9 @@ rem Copy the script to a place that can be accessed both by the user and the adm
 rem and then run it with powershell
 rem ================================================
 
-set directory="%homedrive%%homepath%\Documents"
-set script_name="enable_winrm.ps1"
+set directory=%homedrive%%homepath%\Documents
+set script_name=enable_winrm.ps1
 
 copy "%cd%\%script_name%" "%directory%"
-Powershell.exe -ExecutionPolicy bypass -File "%directory%\\%script_name%"
+Powershell.exe -ExecutionPolicy bypass -File "%directory%\%script_name%"
 pause
