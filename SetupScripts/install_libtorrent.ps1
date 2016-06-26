1cd53eaa06de5759f11589d1af89b7d976f8a257
Lukasz Taczuk <overfl0@users.noreply.github.com>
2016-06-26 22:46:08 +0200

    Bump libtorrent to 1.0.9

--- SetupScripts/install_libtorrent.ps1
+++ SetupScripts/install_libtorrent.ps1
@@ -1,9 +1,9 @@
 $PackageHumanName = "Libtorrent"
 $PackageInstalledCheck = 'C:\Python27\Lib\site-packages\libtorrent.pyd'  # If path exists, exit the script
 
-$PackageUrl = 'https://github.com/arvidn/libtorrent/releases/download/libtorrent-1_0_8/python-libtorrent-1.0.8.win32.msi'
+$PackageUrl = 'https://github.com/arvidn/libtorrent/releases/download/libtorrent-1_0_9/python-libtorrent-1.0.9.win32.msi'
 $PackageDownloadPath = 'c:\vagrant\Downloads'
-$PackageInstallerFileName = 'python-libtorrent-1.0.8.win32.msi'
+$PackageInstallerFileName = 'python-libtorrent-1.0.9.win32.msi'
 $PackageInstaller = Join-Path $PackageDownloadPath $PackageInstallerFileName
 
 
