408184cadb579cdb25f72aea0b422c1f8bad3c73
Lukasz Taczuk <overfl0@users.noreply.github.com>
2016-10-14 23:15:04 +0200

    Signing script

---
+++ sign.bat
@@ -0,0 +1,9 @@
+@echo off
+set "psCommand=powershell -Command "$pword = read-host 'Enter Password' -AsSecureString ; ^
+    $BSTR=[System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($pword); ^
+        [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)""
+for /f "usebackq delims=" %%p in (`%psCommand%`) do set password=%%p
+
+@rem Get signtool at: https://www.microsoft.com/en-us/download/details.aspx?id=8279
+signtool.exe sign /f Signature.p12 /p %password% /t http://time.certum.pl Frontline.exe 
+signtool.exe sign /f Signature.p12 /p %password% /t http://time.certum.pl TB_Launcher.exe 
