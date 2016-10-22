f22e33c6a502c51a510bb227c95457d1900467b7
Lukasz Taczuk <overfl0@users.noreply.github.com>
2016-10-22 23:53:59 +0200

    Updated git because there were issues installing mockfs with the old version

--- SetupScripts/install_msysgit.ps1
+++ SetupScripts/install_msysgit.ps1
@@ -1,9 +1,9 @@
 $PackageHumanName = "Msysgit"
 $PackageInstalledCheck = 'C:\Program Files\Git\bin\git.exe'  # If path exists, exit the script
 
-$PackageUrl = 'https://github.com/msysgit/msysgit/releases/download/Git-1.9.5-preview20141217/Git-1.9.5-preview20141217.exe'
+$PackageUrl = 'https://github.com/git-for-windows/git/releases/download/v2.10.1.windows.1/Git-2.10.1-32-bit.exe'
 $PackageDownloadPath = 'c:\vagrant\Downloads'
-$PackageInstallerFileName = 'Git-1.9.5-preview20141217.exe'
+$PackageInstallerFileName = 'Git-2.10.1-32-bit.exe'
 $PackageInstallerFilePath = Join-Path $PackageDownloadPath $PackageInstallerFileName
 $PackageInstallerArguments = "/SILENT /LOADINF=c:\vagrant\Configs\git_install_config.inf"
 $PackageInstallerExecutable = "$PackageInstallerFileName"
